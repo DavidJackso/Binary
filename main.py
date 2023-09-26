80418af695a5b8fa51acd4c2654ab37bfca1632d
Joseph <gutnov3643@gmail.com>
2023-09-26 23:04:52 +0300

    v4

--- main.py
+++ main.py
@@ -5,16 +5,30 @@ twob = str(bin(int(twop))[2:])
 sum  = ""
 i = -7
 b = 0
+if onep[0] =='-':
+    oneb = oneb[1:]
+if twop[0] =='-':
+    twob = twob[1:]
 
 def x8(a):
-    while len(a) < 8:
+    while len(a) <= 7:
         a = '0' + a
     return a
 
-
 oneb = x8(oneb)
 twob = x8(twob)
-while i < 0:
+
+if onep[0] == '-':
+    oneb = oneb.replace('1','*')
+    oneb = oneb.replace('0','1')
+    oneb = oneb.replace('*','0')
+print(twob)
+if twop[0] == '-':
+    twob = twob.replace('1','*')
+    twob = twob.replace('0','1')
+    twob = twob.replace('*','0')
+print(twob)
+while i <= 0:
    a = int(oneb[-i]) + int(twob[-i]) + b
    if a == 1:
        sum = "1" + sum
@@ -26,5 +40,10 @@ while i < 0:
        sum = "0" + sum
        b = 0
    i = i + 1
+
+if sum[0] == '1':
+    sum = sum.replace('1','*')
+    sum = sum.replace('0','1')
+    sum = sum.replace('*','0')
 print(sum)
 
