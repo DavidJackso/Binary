d64876e8a3a89d182f88886091273d1586202fcc
DavidJackso <51014076+DavidJackso@users.noreply.github.com>
2023-09-26 22:43:22 +0300

    Update main.py
    v2

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ oneb = str(bin(int(onep))[2:])
 twob = str(bin(int(twop))[2:])
 sum  = ""
 i = -7
-
+b = 0
 
 def x8(a):
     while len(a) < 8:
@@ -15,11 +15,16 @@ def x8(a):
 oneb = x8(oneb)
 twob = x8(twob)
 while i < 0:
-   a = int(oneb[-i]) + int(twob[-i])
+   a = int(oneb[-i]) + int(twob[-i]) + b
    if a == 1:
        sum = "1" + sum
-   else:
+       b = 0
+   elif a == 2:
        sum = "0" + sum
+       b = 1
+   elif a == 0:
+       sum = "0" + sum
+       b = 0
    i = i + 1
 print(sum)
 
